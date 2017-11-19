242a6c12d1033ecfabc408cdd43e3e60949b4899
Lajos Koszti <ajnasz@ajnasz.hu>
2017-11-19 15:33:13 +0100

    Use native canvas methods, remove fabric.js

--- kotta.js
+++ kotta.js
@@ -1,13 +1,35 @@
-(function (fabric) {
+(function () {
 	const KOTTA_CONTAINER_ID = 'KottaImg';
 	const CIRCLE_SIZE = 6;
 	const LINE_GAP = 15;
 	const BASE_LINE_MIN = -0.5;
-	const BASE_LINE_MAX = 4;
+	const BASE_LINE_MAX = 4.5;
 	const FONT_SIZE = 16;
 	const NOTE_LINE_SIZE = LINE_GAP * 3;
 	const LINE_INDENT = 10;
 	const FIRST_NOTE_INDENT = 20;
+	var ctx;
+
+	function text(text, { top, left, fontSize, fill }) {
+		ctx.font = `${fontSize}px serif`;
+		const fillStyle = ctx.fillStyle;
+		ctx.fillStyle = fill;
+		ctx.fillText(text, left, top);
+		ctx.fillStyle = fillStyle;
+	}
+
+	function circle({ radius, top, left }) {
+		ctx.beginPath();
+		ctx.arc(left + radius, top + radius, radius, 0, 360);
+		ctx.fill();
+	}
+
+	function line([x1, y1, x2, y2]) {
+		ctx.beginPath();
+		ctx.moveTo(x1, y1);
+		ctx.lineTo(x2, y2);
+		ctx.stroke();
+	}
 
 	// const allNotes = [
 	// 	// -4, -3.5, -3, -2.5, -2,
@@ -61,35 +83,51 @@
 		return pos > BASE_LINE_MIN && pos < BASE_LINE_MAX;
 	}
 
+	function isBelowBase(pos) {
+		return pos > BASE_LINE_MAX;
+	}
+
+	function isAboveBase(pos) {
+		return pos < BASE_LINE_MIN;
+	}
+
 	function createNote([pos, name], index) {
 		const top = (pos + 6) * LINE_GAP + LINE_GAP / 2 - CIRCLE_SIZE;
 		const left = FIRST_NOTE_INDENT + index * CIRCLE_SIZE * 4;
 		const output = [];
 
-		output.push(new fabric.Circle({
+		output.push(circle({
 			radius: CIRCLE_SIZE,
 			fill: 'black',
 			top,
 			left,
+			selectable: false,
 		}));
-		output.push(new fabric.Text(name, {
+		output.push(text(name, {
 			fill: '#666',
-			top: top + FONT_SIZE,
+			top: top + FONT_SIZE + CIRCLE_SIZE + 6,
 			left,
-			fontSize: FONT_SIZE
+			fontSize: FONT_SIZE,
+			selectable: false,
 		}));
 
 		if (pos > 1.5) {
-			output.push(new fabric.Line([90, NOTE_LINE_SIZE, 90, 0], {
-				top: top + CIRCLE_SIZE - LINE_GAP * 3,
-				left: left + CIRCLE_SIZE * 2,
+			const x1 = left + CIRCLE_SIZE * 2;
+			const x2 = x1;
+			const y1 = top + CIRCLE_SIZE - LINE_GAP * 3;
+			const y2 = y1 + NOTE_LINE_SIZE;
+			output.push(line([x1, y1, x2, y2], {
 				stroke: 'black',
+				selectable: false,
 			}));
 		} else {
-			output.push(new fabric.Line([90, NOTE_LINE_SIZE, 90, 0], {
-				top: top + CIRCLE_SIZE,
-				left,
+			const x1 = left;
+			const y1 = top + CIRCLE_SIZE + NOTE_LINE_SIZE;
+			const x2 = x1;
+			const y2 = y1 - NOTE_LINE_SIZE;
+			output.push(line([x1, y1, x2, y2], {
 				stroke: 'black',
+				selectable: false,
 			}));
 		}
 
@@ -105,15 +143,21 @@
 			linePos += 0.5;
 
 			if ((linePos % 1) !== 0 ||
+				isBaseNote(linePos) ||
 				(pos < 0 && (linePos <= pos || linePos > 3.5)) ||
 				(pos > 3.5 && (linePos < 0 ||  linePos >= pos + 1))
 			) {
 				continue;
 			}
-			lines = lines.concat(new fabric.Line([0, 0, CIRCLE_SIZE * 3, 0], {
-				left: left - CIRCLE_SIZE / 2,
-				top: (linePos + 6) * LINE_GAP + LINE_GAP / 2 - CIRCLE_SIZE - (LINE_GAP / 2 - CIRCLE_SIZE),
+
+			const x1 = left - CIRCLE_SIZE / 2;
+			const x2 = x1 + CIRCLE_SIZE * 3;
+			const y1 = (linePos + 6) * LINE_GAP + LINE_GAP / 2 - CIRCLE_SIZE - (LINE_GAP / 2 - CIRCLE_SIZE);
+			const y2 = y1;
+
+			lines = lines.concat(line([x1, y1, x2, y2], {
 				stroke: 'black',
+				selectable: false,
 			}));
 		}
 
@@ -129,24 +173,23 @@
 			return canvas;
 		})());
 
-		const canvas = new fabric.Canvas(KOTTA_CONTAINER_ID);
 
-		const lines = Array.from(new Array(5)).map((_, index) => {
-			return new fabric.Line([0, 0, contentWidth - LINE_INDENT * 2, 0], {
-				left: LINE_INDENT,
-				top: (index + 6) * LINE_GAP,
+		ctx = document.getElementById(KOTTA_CONTAINER_ID).getContext('2d');
+
+		Array.from(new Array(5)).map((_, index) => {
+			const x1 = LINE_INDENT;
+			const x2 = contentWidth - LINE_INDENT * 2 + LINE_INDENT;
+			const y = (index + 6) * LINE_GAP;
+
+			return line([x1, y, x2, y], {
 				stroke: 'black',
+				selectable: false,
 			});
 		});
 
-		canvas.add(...lines);
-
 		const numberOfNotes = Math.round((contentWidth - LINE_INDENT * 2 - FIRST_NOTE_INDENT) / (CIRCLE_SIZE * 4));
-		const toDraw = Array.from(new Array(numberOfNotes))
+		Array.from(new Array(numberOfNotes))
 			.map(() => selectedNotes[getRandomIntInclusive(0, selectedNotes.length - 1)])
-			.map(createNote)
-			.reduce((o, h) => o.concat(h), []);
-		canvas.add(...toDraw);
-		canvas.renderAll();
+			.map(createNote);
 	});
-}(window.fabric));
+}());
